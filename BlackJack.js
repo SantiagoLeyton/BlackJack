d09d13507c00d1738919deabd48e5cd695e66b49
Santiago Leyton <sleyton_644@unihumboldt.edu.co>
2025-03-11 22:57:32 -0500

    Upgraded function hit

--- BlackJack.js
+++ BlackJack.js
@@ -1,7 +1,7 @@
-let stop = 17;
-let final = 21;
+let cardsPR; 
+let cardsDR;
 let cardsP = [ ];
-let cardsD = [ ]; //those arrays are empty because here we will add the player and delaer cards
+let cardsD = [ ]; //those arrays are empty because here we will add the player and delaer's cards
 let cards = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
@@ -26,23 +26,52 @@ function start() {
     //here the dealer and the player will have two cards to start the game
 
     document.getElementById("hitBtn").classList.remove("hidden-f");
-    document.getElementById("standBtn").classList.remove("hidden-f");
     document.getElementById("hitBtn").classList.add("show-s");
-    document.getElementById("standBtn").classList.add("show-s");
+
+    document.getElementById("standBtn").classList.remove("hidden-f");
+    document.getElementById("standBtn").classList.add("show-s"); 
+   
     document.getElementById("startBtn").classList.add("hidden-f");
     //here the code will hide the start button and show hit and stand buttons
 
-    showCards("Player", cardsP);
+    cardsPR = showCards("Player", cardsP);
     document.getElementById("Dealer").innerHTML = cardsD[0] + ", ?";
     //In the BlackJack rules, the second card of the dealer is hidden while the player are playing, when he/she stop, we can watch the dealer cards
 };
 
-document.getElementById("hitBtn").addEventListener("click", function() {
-    cardsP.push(card());
-    showCards("Player", cardsP);
-});
+function hit() {
+    let total = cardsP.reduce((acc, num) => acc + num);
+    //here we used .reduce to know the sum of the player's cards
 
-document.getElementById("standBtn").addEventListener("click", function() {
-    showCards("Player", cardsP);
-    showCards("Dealer", cardsD);
-});
+    if(total <= 20) {
+        if(total >= 18){ // Here I tried to warn the player that he has a lot of cards
+            while(true) {
+                let question = parseInt(prompt(`Now you've cards that add up a too high value\nDo you wanna continue?\n\n1. Yes\n2. No`));
+                if(question == 1){
+                    cardsP.push(card());
+                    cardsPR = showCards("Player", cardsP);
+                    break;
+                } else if(question == 2 || question == null){
+                    // If the user click "cancel" in the prompt, question will be the same as null
+                    cardsPR = showCards("Player", cardsP);
+                    break;
+                } else {
+                    // If the user write an invalid option, the "while" will start again 
+                    alert('select an option');
+                };
+            };
+        } else {
+            cardsP.push(card());
+            showCards("Player", cardsP);
+        };
+    };
+};
+
+function stand() {
+    cardsDR = showCards("Dealer", cardsD);
+}
+
+document.getElementById('startBtn').addEventListener("click", start)
+document.getElementById('hitBtn').addEventListener("click", hit)
+document.getElementById('standBtn').addEventListener("click", stand)
+document.getElementById('restartBtn').addEventListener("click", ) /* FIX HERE TOO */
